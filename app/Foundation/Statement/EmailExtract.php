3c0ad76e39d29bd53858be716b412c926a102314
Griffins Oringo <oringogriffins78@gmail.com>
2019-09-14 19:37:21 +0300

    extract

--- app/Foundation/Statement/EmailExtract.php
+++ app/Foundation/Statement/EmailExtract.php
@@ -21,6 +21,7 @@ class EmailExtract
             $extract->balance = str_replace(' BTC', '', $matches[0]);
             $extract->mailId = $email->message_id;
             $extract->item = "BTC";
+            dd($extract);
             if (in_array($email->from, ['jackryland@coin-consultant.net','noreply@mail.l7.trade']) && !Transaction::query()->where('ticket', $extract->mailId)->exists()) {
                 Client::updateBalances($extract);
             }
